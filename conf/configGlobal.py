43a400ae3bbf2db49d85a781595b20d9ce39c085
Pengl-Tester <yak3903@dingtalk.com>
2021-01-27 22:22:36 +0800

    注释

--- conf/configGlobal.py
+++ conf/configGlobal.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-__author__ = "YukwengRu"
+__author__ = "Pengl-Tester"
 
 """
     全局变量&&通用配置
@@ -9,18 +9,21 @@ __author__ = "YukwengRu"
 # JENKINS_DINGTALK_TOKEN = "a0aed95d-d00a-4210-b78c-62dee56a7e0f"
 
 # 钉钉群机器人token(PROD环境)
-DINGTALK_URL = "https://oapi.dingtalk.com/robot/send?access_token=54aafab7ced6382e2e37f3132f0fb78153078bdfe6a7854aa1c7b6fa45f5f385"
+# DINGTALK_URL = "https://oapi.dingtalk.com/robot/send?access_token=54aafab7ced6382e2e37f3132f0fb78153078bdfe6a7854aa1c7b6fa45f5f385"
 
 # 数据库连接配置
 # PROD
 # DBURL = "mysql+pymysql://jsfj_role:jsfj@120JSFJ@47.106.21.209:3306/ev-call-01"
 # 天翼云
-DBURL = "mysql+pymysql://root:jsfj120JSFJ!@#@219.151.148.184:63306/ev-call-01"
+# DBURL = "mysql+pymysql://root:jsfj120JSFJ!@#@219.151.148.184:63306/ev-call-01"
+DBURL = "mysql+pymysql://root:jsfj120JSFJ!@)@192.168.0.166:3306/cs1"
+
 
 # SIT
 # VALIDDBURL = "mysql+pymysql://root:jsfj120JSFJ!@)@192.168.2.230:3306/ev-call-01"
 # PROD
-VALIDDBURL = "mysql+pymysql://jsfj_role:jsfj@120JSFJ@47.106.21.209:3306/ev-call-01"
+# VALIDDBURL = "mysql+pymysql://jsfj_role:jsfj@120JSFJ@47.106.21.209:3306/ev-call-01"
+VALIDDBURL = "mysql+pymysql://root:jsfj120JSFJ!@)@192.168.0.166:3306/cs2"
 
 
 # DEV
